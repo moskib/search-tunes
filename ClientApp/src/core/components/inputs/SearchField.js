a22cdd1404af73e4f1c11e1b530a34d7eb627b11
moskib <moshekiba3@gmail.com>
2020-01-27 21:08:24 +0200

    Added Enter for search bar

--- ClientApp/src/core/components/inputs/SearchField.js
+++ ClientApp/src/core/components/inputs/SearchField.js
@@ -2,6 +2,12 @@ import React from 'react';
 import { InputGroup, Input, Button, InputGroupAddon } from 'reactstrap';
 
 const SearchField = props => {
+  const _handleKeyDown = e => {
+    if (e.key === 'Enter') {
+      props.click();
+    }
+  };
+
   return (
     <form className='user'>
       <InputGroup>
@@ -10,6 +16,7 @@ const SearchField = props => {
           className='form-control-user'
           value={props.value}
           onChange={props.handleChange}
+          onKeyDown={_handleKeyDown}
         />
         <InputGroupAddon addonType='append'>
           <Button color='primary' className='btn-user' onClick={props.click}>
