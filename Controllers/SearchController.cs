28f2fcd1cb738a9e7f9fbcf04d4f7a0bf1041aea
moskib <moshekiba3@gmail.com>
2020-01-27 14:21:44 +0200

    Added data transfer object for search reasults

--- Controllers/SearchController.cs
+++ Controllers/SearchController.cs
@@ -1,11 +1,12 @@
 ﻿using System;
+using System.Collections;
 using System.Net.Http;
 using System.Text.Json;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
-
+using search_tunes.Entities.Dtos;
 
 namespace search_tunes.Controllers
 {
@@ -40,8 +41,10 @@ namespace search_tunes.Controllers
                 var responseStream = await response.Content.ReadAsStringAsync();
 
                 var obj = (JObject)JsonConvert.DeserializeObject(responseStream);
+
+                var searchResults = obj["results"].ToObject<SearchResult[]>();
                 
-                return Ok(obj["results"]);
+                return Ok(searchResults);
             }
             else
             {
--- Entities/Dtos/SearchResult.cs
+++ Entities/Dtos/SearchResult.cs
@@ -3,6 +3,11 @@ namespace search_tunes.Entities.Dtos
 {
     public class SearchResult
     {
-        
+        public string Kind { get; set; }
+        public int TrackId { get; set; }
+        public string TrackName { get; set; }
+        public string PreviewUrl { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string ArtWorkUrl100 { get; set; }
     }
 }
